79805ce647b4d99a5eac83a80f559a59941ea659
Dave Tyson <davetysonbass@gmail.com>
2023-03-10 18:46:24 -0500

    added initial box styling and menu js

---
+++ script.js
@@ -0,0 +1,18 @@
+const menuButton = document.querySelector('.rotateText');
+const overallMenu = document.querySelector('.menu');
+// const showMenu = function(){
+//     const menu = document.querySelector('ul');
+//     menu.style.display="static";
+//     console.log('you clicked the menu button');
+// }
+
+// menuButton.addEventListener('click', showMenu());
+
+menuButton.addEventListener('mouseover', function(){
+    const menu = document.querySelector('ul');
+    menu.style.opacity='100%';
+    overallMenu.addEventListener('mouseleave', function(){
+        // const menu = document.querySelector('ul');
+        menu.style.opacity='0%';
+    });
+});
