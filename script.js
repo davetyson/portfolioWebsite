7a646ff1cc39c09674eba106ddbca921c2f3dc1d
Dave Tyson <davetysonbass@gmail.com>
2023-03-12 11:18:48 -0400

    created new html pages & sass partials

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 const menuButton = document.querySelector('.rotateText');
 const overallMenu = document.querySelector('.menu');
-// const showMenu = function(){
-//     const menu = document.querySelector('ul');
-//     menu.style.display="static";
-//     console.log('you clicked the menu button');
-// }
-
-// menuButton.addEventListener('click', showMenu());
+const footerP = document.querySelector('.footerP');
+let currentYear = new Date().getFullYear();
+footerP.innerHTML = `© Dave Tyson ${currentYear}`;
 
 menuButton.addEventListener('mouseover', function(){
     const menu = document.querySelector('ul');
     menu.style.opacity='100%';
-    // menu.style.transform='translateX(0%)';
     menu.classList='runMenuAnimation';
     overallMenu.addEventListener('mouseleave', function(){
-        // const menu = document.querySelector('ul');
         menu.style.opacity='0%';
         menu.classList='menuSlideBack';
-        // menu.style.transform='translateX(-50%)';
     });
 });
