705890849844fec73c401026fc3de63089b5e1f7
Dave Tyson <davetysonbass@gmail.com>
2023-03-13 15:53:31 -0400

    added my own photo, cleaned up some styling

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const overallMenu = document.querySelector('.menu');
 const footerP = document.querySelector('.footerP');
 let currentYear = new Date().getFullYear();
 footerP.innerHTML = `© Dave Tyson ${currentYear}`;
+const projectList = document.querySelector('.projects');
 
 menuButton.addEventListener('mouseover', function(){
     const menu = document.querySelector('ul');
@@ -13,3 +14,26 @@ menuButton.addEventListener('mouseover', function(){
         menu.classList='menuSlideBack';
     });
 });
+
+// setTimeout(() => {
+//      projectList.style='overflow: hidden';
+// }, "300");
+
+// setTimeout(() => {if (projectList.scrollHeight >= 717) {
+//     // projectList.style='overflow: auto'
+//     projectList.style='justify-content: end';
+// }
+// else {
+//     // // projectList.style='overflow: auto'
+//     // projectList.classList='mainSlideUp projects flexbox';
+// }
+// }, "301");
+
+  
+
+//scroll height 717
+//what if I say...
+    // if scroll height >= 717 then
+        // apply class of justify-content: end
+    // else
+        // remove class of justify-content: end
