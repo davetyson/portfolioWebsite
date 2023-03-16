e93cbfeaa054f316d68a1d76e631604879867beb
Dave Tyson <davetysonbass@gmail.com>
2023-03-16 17:47:25 -0400

    added contact form with Formspree, should work well and has styles/responsiveness

--- script.js
+++ script.js
@@ -24,6 +24,22 @@ portfolioApp.footerYear = () => {
     portfolioApp.footerP.innerHTML = `© Dave Tyson ${currentYear}`;
 }
 
+// Function to clear contact section on page load
+portfolioApp.clearContact = () => {
+    portfolioApp.name.value = '';
+    portfolioApp.email.value = '';
+    portfolioApp.textArea.value = '';
+}
+
+// Formspree code to clear form after submission
+portfolioApp.formspreeClear = () => {
+    window.onbeforeunload = () => {
+        for(const form of document.getElementsByTagName('form')) {
+          form.reset();
+        }
+    }
+}
+
 // Initialize variables and functions needed for script
 portfolioApp.init = () => {
     portfolioApp.menuButton = document.querySelector('.rotateText');
@@ -31,8 +47,14 @@ portfolioApp.init = () => {
     portfolioApp.footerP = document.querySelector('.footerP');
     portfolioApp.menu = document.querySelector('ul');
     portfolioApp.projects = document.querySelector('projects');
+    portfolioApp.name = document.querySelector('#name');
+    portfolioApp.email = document.querySelector('#email');
+    portfolioApp.textArea = document.querySelector('textarea');
+    portfolioApp.form = document.querySelector('form');
     portfolioApp.menuStart();
     portfolioApp.footerYear();
+    portfolioApp.clearContact();
+    portfolioApp.formspreeClear();
 }
 
 // Initialize portfolioApp
